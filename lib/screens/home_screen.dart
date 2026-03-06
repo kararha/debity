bca11f09a3408cb64b86b74fea02c0ead00abeb5
Karar Haider <karar55156@gmail.com>
2026-03-06 23:07:34 +0300

    Add localization import to home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../api/api_service.dart';
 import 'auth/auth_screen.dart';
+import '../core/l10n/app_localizations.dart';
 import '../core/theme/app_colors.dart';
 import '../core/theme/app_spacing.dart';
 import '../core/theme/app_text_styles.dart';
