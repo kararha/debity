faa4ae7f98e5da4f3813b352c50efdd8452b1402
Karar Haider <karar55156@gmail.com>
2026-03-08 23:00:09 +0300

    Remove unused url_launcher import from customer_details_screen.dart for code cleanup

--- lib/screens/customers/customer_details_screen.dart
+++ lib/screens/customers/customer_details_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:url_launcher/url_launcher.dart';
 import '../../core/theme/app_colors.dart';
 import '../../core/theme/app_spacing.dart';
 import '../../core/theme/app_text_styles.dart';
